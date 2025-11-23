64cf7fe99709ee58340df220c1bd046dde8c3491
motasimdev <themotasimdev@gmail.com>
2025-11-23 09:37:21 +0600

    sdfsdf

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 4, // একসাথে 4 slide
-  spaceBetween: 20, // slide এর মধ্যে gap
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    0: { slidesPerView: 1 },
-    576: { slidesPerView: 2 },
-    992: { slidesPerView: 4 },
-  },
-});
+
+
+
